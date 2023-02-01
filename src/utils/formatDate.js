beb5139526b0faf97070e276bf57fbcbb9be3d4d
Jeremy Jeremiah <jeremyjeremiah587@gmail.com>
2023-02-01 13:08:10 -0700

    breadcrumbs before workshop2

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,5 @@
 export const formatDate = (date) =>{
-    return new Intl.DateTimeFormat('en-US',{
+    return new Intl.DateTimeFormat('en-US', {
         year:'numeric',
         month:'short',
         day:'2-digit'
